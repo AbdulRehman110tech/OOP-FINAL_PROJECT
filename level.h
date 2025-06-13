d56776f02249eff32d8d6700df76de9c4c3d2307
AbdulRehman110tech <bscs24149@itu.edu.pk>
2025-06-14 02:15:10 +0500

    2 : 14

--- level.h
+++ level.h
@@ -6,3 +6,5 @@
 #include "player.h"
 #include "enemy.h"
 
+// enemies and one boss player not needed just initialize void work logic level array in game maneger status of level locked or un locked an be saved in file and 
+// data present on main can also be saved if needed texture needed of level menue also needed which level to play in manager 
